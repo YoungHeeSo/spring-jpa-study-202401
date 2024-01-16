60a44173e6459e01cc604d3673320715a5deb1df
YoungHeeSo <apdlfdldjqtm@gmail.com>
2024-01-16 10:49:49 +0900

    # JPA 단방향 연관관계

---
+++ src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.study.jpa.chap04_relation.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_emp")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private Long id;
+
+    @Column(name = "emp_name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dept_id")
+    private Department department;
+
+
+}
